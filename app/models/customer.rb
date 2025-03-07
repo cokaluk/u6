98b77b4509c0505174f7a4c03667570fbc34cc3a
cokaluk <cokaluk@academic.rrc.ca>
2025-03-07 17:43:10 -0600

    initial commit

---
+++ app/models/customer.rb
@@ -0,0 +1,9 @@
+class Customer < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email_address", "full_name", "id", "notes", "phone_number", "updated_at"]
+  end
+
+  validates :full_name, presence: true
+  validates :phone_number, presence: true
+
+end
